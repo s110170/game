766ff339c75ed39c9289dfc62ba133499d788f8d
s110170 <131328706+s110170@users.noreply.github.com>
2023-05-03 22:05:30 +0800

    更改移動錯誤

--- main.ts
+++ main.ts
@@ -5,10 +5,10 @@ input.onGesture(Gesture.LogoDown, function () {
     bear.change(LedSpriteProperty.Y, -1)
 })
 input.onGesture(Gesture.TiltLeft, function () {
-    bear.change(LedSpriteProperty.X, 1)
+    bear.change(LedSpriteProperty.X, -1)
 })
 input.onGesture(Gesture.TiltRight, function () {
-    bear.change(LedSpriteProperty.X, -1)
+    bear.change(LedSpriteProperty.X, 1)
 })
 let bear: game.LedSprite = null
 let honey = game.createSprite(randint(0, 4), randint(0, 4))
