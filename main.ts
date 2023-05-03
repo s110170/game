11826d0729ea7b5d940b74e7c7a81ca2c5d54388
s110170 <131328706+s110170@users.noreply.github.com>
2023-05-03 21:57:07 +0800

    新增移動方式

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
+input.onGesture(Gesture.LogoUp, function () {
+    bear.change(LedSpriteProperty.Direction, 1)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    bear.change(LedSpriteProperty.Y, -1)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    bear.change(LedSpriteProperty.X, 1)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    bear.change(LedSpriteProperty.X, -1)
+})
+let bear: game.LedSprite = null
 let honey = game.createSprite(randint(0, 4), randint(0, 4))
-let bear = game.createSprite(3, 3)
+bear = game.createSprite(3, 3)
 basic.forever(function () {
 	
 })
